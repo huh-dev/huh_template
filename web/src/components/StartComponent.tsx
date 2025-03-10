dbb7640fee4fe05acadaa929bfb19cf0b80e883a
huh <122040036+huh-dev@users.noreply.github.com>
2025-03-10 10:55:02 +0100

    Updated structure and removed fontawesome lib

--- web/src/components/StartComponent.tsx
+++ web/src/components/StartComponent.tsx
@@ -1,23 +1,12 @@
 import { useLocale } from "../providers/LocaleProvider";
+import styles from "../styles/StartComponent.module.scss"
 
 export default function StartComponent() {
 
   const { translations } = useLocale();
 
   return (
-    <div style={{
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.7)',
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      color: 'white',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: '24px'
-    }}>
+    <div className={styles.startComponent}>
       {translations.ui_back}
     </div>
   );
